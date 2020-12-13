fd7c0704b8485dcaef4d2191d11a115288b3b779
chaeyeon-vatech <chaeyeon.park@vatech.com>
2020-12-13 22:44:33 +0900

    Troubleshooting Errors after merge

--- client/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-    <div className="footer">
-        © Vatech
-    </div>
-)
-
-export default Footer;
--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
-import { ApolloServer } from 'apollo-server';
 import typeDefs from './graphql/schema/index.js';
 import resolvers from './graphql/resolvers/index.js';
 
 import dotenv from 'dotenv';
+
+import pkg from 'apollo-server';
+const {ApolloServer} = pkg;
+
 dotenv.config(); // 환경 변수를 찾아와 사용할 수 있게 해줌
 
 import dbConnect from './models/index.js';
