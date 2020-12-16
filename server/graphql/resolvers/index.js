aa864e828587b7af8be25c0c1053d65482c54479
chaeyeon-vatech <chaeyeon.park@vatech.com>
2020-12-16 15:22:36 +0900

    Merge with Master

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,51 +1,58 @@
 import Content from '../../models/content.js';
-
+import users from '../../models/user.js';
+import bcrypt from 'bcryptjs';
+import jsonwebtoken from 'jsonwebtoken';
 // resolver에서 mutation을 정의하고 구현하는 걸 보니 가장 중요한 부분이 아닐까 싶다. service 단이라고 생각하자
 const resolvers = {
     Query: {
         async contents(_, args) {
             try {
-                const contents = await Content.find().sort({createdAt: -1});
+                const contents = await Content.find().sort({createdAt:-1});
                 const search = args.search || "";
                 const index = args.index;
                 const hasNext = args.hasNext;
                 let result = []
-                if (args.category == 1) {
+                if ( args.category == 1) {
 
-                    for (let i = 0; i < contents.length; i++) {
+                    for (let i=0; i < contents.length; i++ ) {
 
                         if (contents[i].title.toLowerCase().indexOf(search.toLowerCase()) > -1) {
                             result.push(contents[i]);
                         }
                     }
-                    if (hasNext == false) {
-                        result = result.slice(10 * (index - 1), result.length);
-                    } else {
-                        result = result.slice(10 * (index - 1), 10 * (index));
+                    if(hasNext == false){
+                        result = result.slice(10*(index-1), result.length);
                     }
-                } else if (args.category == 2) {
-                    for (let i = 0; i < contents.length; i++) {
+                    else{
+                        result = result.slice(10*(index-1), 10*(index));
+                    }
+                }
+                else if(args.category == 2){
+                    for (let i=0; i < contents.length; i++ ) {
 
                         if (contents[i].content.toLowerCase().indexOf(search.toLowerCase()) > -1) {
                             result.push(contents[i]);
                         }
                     }
-                    if (hasNext == false) {
-                        result = result.slice(10 * (index - 1), result.length);
-                    } else {
-                        result = result.slice(10 * (index - 1), 10 * (index));
+                    if(hasNext == false){
+                        result = result.slice(10*(index-1), result.length);
                     }
-                } else {
-                    for (let i = 0; i < contents.length; i++) {
+                    else{
+                        result = result.slice(10*(index-1), 10*(index));
+                    }
+                }
+                else {
+                    for (let i=0; i < contents.length; i++ ) {
 
                         if (contents[i].content.toLowerCase().indexOf(search.toLowerCase()) > -1 || contents[i].title.toLowerCase().indexOf(search.toLowerCase()) > -1) {
                             result.push(contents[i]);
                         }
                     }
-                    if (hasNext == false) {
-                        result = result.slice(10 * (index - 1), result.length);
-                    } else {
-                        result = result.slice(10 * (index - 1), 10 * (index));
+                    if(hasNext == false){
+                        result = result.slice(10*(index-1), result.length);
+                    }
+                    else{
+                        result = result.slice(10*(index-1), 10*(index));
                     }
                 }
                 return result;
@@ -54,6 +61,29 @@ const resolvers = {
                 throw err;
             }
         },
+        // 로그인 되어 있는 나
+        async me(_, args, { user }) {
+            if(!user) throw new Error('You are not authenticated')
+            return await users.findById(user.id)
+        },
+        // id로 검색
+        async user(root, { id }, { user }) {
+            try {
+                if(!user) throw new Error('You are not authenticated!')
+                return users.findById(id)
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        },
+        // 모든 유저 검색
+        async allUsers(root, args, { user }) {
+            try {
+                if (!user) throw new Error('You are not authenticated!')
+                return users.find()
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        }
     },
     Content: {
         _id(_, args) {
@@ -61,7 +91,6 @@ const resolvers = {
         },
         title(_, args) {
             return _.title;
-            ß
         },
         content(_, args) {
             return _.content;
@@ -90,7 +119,7 @@ const resolvers = {
                 throw new Error('Error: ', e)
             }
         },
-        updateContent: async (_, {_id, title, content}) => {
+        updateContent: async (_, {_id,title,content}) => {
             try {
                 const updatedContent = await Content.findByIdAndUpdate(_id, {
                     $set: {title, content}
@@ -107,6 +136,67 @@ const resolvers = {
             } catch (e) {
                 throw new Error('Error: ', e)
             }
+        },
+        // 회원가입
+        registerUser: async (root, { username, email, password })=> {
+            try {
+                // 이메일 중복 체크
+                const userConfirm = await users.findOne({ email : email})
+                if(userConfirm != null){
+                    return "Already registered Email. Please try again.";
+                }
+                const user = await users.create({
+                    username,
+                    email,
+                    password: await bcrypt.hash(password, 10)
+                })
+                const token = jsonwebtoken.sign(
+                    { id: user.id, email: user.email},
+                    "somereallylongsecret",
+                    { expiresIn: '1y' }
+                )
+                return {
+                    token, id: user.id, username: user.username, email: user.email, message: "Authentication succesfull"
+                }
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        },
+        login: async(_, { email, password }) => {
+            try {
+                // 유저 이메일 정보 확인 후 로그인
+                const user = await users.findOne({ email : email})
+                console.log(user);
+                console.log(email);
+                if (!user) {
+                    throw new Error('No user with that email')
+                }
+                const isValid = await bcrypt.compare(password, user.password)
+                if (!isValid) {
+                    throw new Error('Incorrect password')
+                }
+                // return jwt
+                const token = jsonwebtoken.sign(
+                    { id: user.id, email: user.email},
+                    "somereallylongsecret",
+                    { expiresIn: '1d'}
+                )
+                return {
+                    token, user
+                }
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        },
+        logout: async(_, __, { user }) => {
+            if(!user) {
+                return false;
+            }
+            else { // 로그인 상태라면(토큰이 존재하면) 토큰 비워주기
+                user.token = '';
+                return true;
+            }
+
         }
     }
 };
--- server/graphql/schema/index.js
+++ server/graphql/schema/index.js
@@ -1,13 +1,25 @@
 import gql from 'graphql-tag'; //gql은 자바스크립트로 스키마를 정의함 이것도 spring model 같음..? 거의 컨트롤러 같은 느낌
 const typeDefs = gql`
     type Query {
-        contents(search:String, category:Int, index:Int, hasNext:Boolean): [Content]
+        contents(search:String, category:Int, index:Int, hasNext:Boolean): [Content]!
+        user(id: ID!): User
+        allUsers: [User!]!
+        me: User
+    }
+    type User {
+        id: ID!
+        username: String
+        email: String!
+    }
+    type AuthPayload {
+        token: String
+        user: User
     }
     type Content {
-        _id: ID
+        _id: ID!
         title: String!
         content: String!
-        createdAt: String
+        createdAt: String!
     }
     input ContentInput{
         title: String!
@@ -18,8 +30,10 @@ const typeDefs = gql`
         updateContent(_id: ID!, title:String, content:String): Content!
         removeContent(_id: ID!): Content!
         searchByID(_id: ID!) : Content!
-
-    }
+        registerUser(username: String, email: String!, password: String!): AuthPayload
+        login (email: String!, password: String!): AuthPayload!
+        logout:Boolean!
+    }s
 `;
 // input 타입은 인자가 적으면 그냥 넣어주면 되지만 만약에 인자 값이 10개가 넘어간다고 했을 때 한번에 넣을 수 있는 객체이다.
 // mutation은 리소스를 변경할 때 만들어주는 것이고 아마 CRUD 구성하는 모든 함수가 여기 들어가지 않을까 싶다.
